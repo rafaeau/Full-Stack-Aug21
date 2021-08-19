71ee36cf25ff016b938569a31525edcaeb2af00b
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-08-19 18:03:02 +0200

    Random number highlighted

--- m1/m1-d9/index.js
+++ m1/m1-d9/index.js
@@ -8,6 +8,7 @@ for (let numbers = 1; numbers <=76; numbers++) {
     newNumber.innerText = numbers
     newNumber.id = numbers
     newNumber.classList.add('numbers')
+
     numbersContainerNode.appendChild(newNumber)
     }
 }
@@ -15,10 +16,26 @@ for (let numbers = 1; numbers <=76; numbers++) {
 function createRandom() {
     return Math.floor(Math.random() * 76 + 1)
 }
+/* 
+let numberBox = document.getElementsByName('number');
+numberBox.classList.add('highlight'); */
+
+function highlightRandom () {
+    const randomNumber = createRandom()
+    document.getElementById(randomNumber).style.border = "3px solid red"
+}
+
+
+createRandom()
+
+
 
 /* function assimilateNumber(){
+    
     let result = ""
+
     for(let i = 0; i < numbers.length; i ++){
+    
         let  = numbers[i]
 } */
 
